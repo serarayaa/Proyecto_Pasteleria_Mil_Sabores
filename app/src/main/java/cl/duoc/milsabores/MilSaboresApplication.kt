6920bdf207a6bc670ef196723318ffd58099f479
serarayaa <ser.arayaa@duocuc.cl>
2025-11-25 16:20:09 -0300

    Configura MilSaboresApplication para Hilt: añade anotación @HiltAndroidApp para habilitar inyección de dependencias

--- app/src/main/java/cl/duoc/milsabores/MilSaboresApplication.kt
+++ app/src/main/java/cl/duoc/milsabores/MilSaboresApplication.kt
@@ -4,7 +4,9 @@ import android.app.Application
 import cl.duoc.milsabores.core.AppLogger
 import cl.duoc.milsabores.service.PedidosObserverService
 import com.google.firebase.FirebaseApp
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MilSaboresApplication : Application() {
     override fun onCreate() {
         super.onCreate()
